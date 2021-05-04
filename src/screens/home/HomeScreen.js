e1887f89b82cf2899f83d7e69a684c3f0cfcc279
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-05-04 15:32:01 +0530

    added reselect

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const HomeScreen = () => {
+  const onMessage = useCallback((trade) => {
+    console.log("trade", trade);
+  }, []);
+
+  useEffect(() => {
+    // const socket = new WebSocket(
+    //   "wss://stream.binance.com:9443/ws/!miniTicker@arr"
+    // );
+    // socket.addEventListener("message", onMessage);
+  }, [onMessage]);
+
   return (
     <View>
       <Text>Home Screen</Text>
