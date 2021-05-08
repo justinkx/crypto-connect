3f77edd09f7d51db6e034c1e25e8fe697c833d85
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-05-08 17:02:14 +0530

    added toast-message dependency

---
+++ src/screens/Trade/TradeScreen.js
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const TradeScreen = ({ navigation, route }) => {
+  return (
+    <View>
+      <Text>Trade Screen</Text>
+    </View>
+  );
+};
+
+export default memo(TradeScreen);
+
+const styles = StyleSheet.create({});
