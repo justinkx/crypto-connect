057e9fc15227c957ba790a6be78b5a165927f4e7
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-05-08 19:49:36 +0530

    added Book selectors

---
+++ src/redux/selectors/book.selector.js
@@ -0,0 +1,27 @@
+import { createSelector } from "reselect";
+import memoizeOne from "memoize-one";
+import _keys from "lodash/keys";
+
+import { createDeepEqualSelector } from "./selector.helper";
+
+export const getReducer = (state) => state.book || {};
+export const getAsks = createSelector(getReducer, (book) => book.ask);
+export const getBids = createSelector(getReducer, (book) => book.bid);
+
+export const getAskPrice = createSelector(getAsks, (asks) => {
+  const askPrices = _keys(asks);
+  return askPrices.sort();
+});
+
+export const getBidPrice = createSelector(getBids, (bids) => {
+  const bidPrices = _keys(bids);
+  return bidPrices.sort().reverse();
+});
+
+export const getAskData = createDeepEqualSelector(getAsks, (asks) =>
+  memoizeOne((price) => asks[price])
+);
+
+export const getBidData = createDeepEqualSelector(getBids, (bids) =>
+  memoizeOne((price) => bids[price])
+);
