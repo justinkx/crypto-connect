d8a5fc937023b7b454c0c430bd83cdbc8d634045
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-05-08 18:56:00 +0530

    usefocusEffect to dispatch book initialize

--- src/screens/Trade/TradeScreen.js
+++ src/screens/Trade/TradeScreen.js
@@ -1,9 +1,24 @@
-import React, { memo } from "react";
+import React, { memo, useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import { useDispatch } from "react-redux";
 
 import GlobalStyle from "../../style/GlobalStyle";
+import {
+  initializeBook,
+  resetBookChannel,
+} from "../../redux/action/book.action";
 
 const TradeScreen = ({ navigation, route }) => {
+  const dispatch = useDispatch();
+
+  useFocusEffect(
+    React.useCallback(() => {
+      dispatch(initializeBook());
+
+      return () => dispatch(resetBookChannel());
+    }, [dispatch])
+  );
   return (
     <View style={GlobalStyle.flex}>
       <Text>Trade Screen</Text>
