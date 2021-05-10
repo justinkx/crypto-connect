5ea574f23df2c0fa3db30ac8a29c1d65e2751178
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-05-10 16:15:43 +0530

    added bash script

--- src/components/Book/BookRow.js
+++ src/components/Book/BookRow.js
@@ -8,24 +8,41 @@ import BookPlaceholder from "./BookPlaceholder";
 
 const QTY_DECIMAL_PLACES = 3;
 
+const priceLayout = {
+  width: 100,
+  height: 14,
+  borderRadius: 2,
+};
+const valueLayout = {
+  width: 60,
+  height: 14,
+  borderRadius: 2,
+};
+
 const BookRow = ({ price, selector, isBid }) => {
   const rowData = useSelector((state) => selector(state)(price), shallowEqual);
 
   // if (!price) return <BookPlaceholder isBid={isBid} />;
 
   return (
-    <SkeletonContent isLoading={!price} containerStyle={styles.rowContainer}>
-      <Text style={[styles.value, !isBid && styles.askColor]}>
-        {isBid
-          ? parseFloat(rowData.quantity).toFixed(QTY_DECIMAL_PLACES)
-          : rowData.price}
-      </Text>
+    <View style={styles.rowContainer}>
+      <SkeletonContent
+        layout={[isBid ? valueLayout : priceLayout]}
+        isLoading={!price}
+      >
+        <Text style={[styles.value, !isBid && styles.askColor]}>
+          {isBid
+            ? parseFloat(rowData.quantity).toFixed(QTY_DECIMAL_PLACES)
+            : rowData.price}
+        </Text>
+      </SkeletonContent>
+
       <Text style={[styles.value, isBid && styles.bidColor]}>
         {isBid
           ? rowData.price
           : parseFloat(rowData.quantity).toFixed(QTY_DECIMAL_PLACES)}
       </Text>
-    </SkeletonContent>
+    </View>
   );
 };
 
